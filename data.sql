57cbd0f0169082c5280199826722ccf7852bc3dd
Saikumar6901 <54203753+Saikumar6901@users.noreply.github.com>
2019-08-19 17:12:51 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,22 +1,66 @@
+create database BankApp;
 
 use BankApp;
-drop table if exists CustomerDetails;
 
-drop table if exists Login;
 
-drop table if exists Transaction;
+create table User_login(
+     userId int primary key,
+     password varchar(15) not null,
+     last_login_time timestamp not null);
+	
+insert into User_Login values(19080001,'Kmit123$',now());
 
-drop table if exists Account;
+create  table Registration(
+      account_no varchar(15) primary key,
+	  pan_no varchar(10) not null,
+	  firstname varchar(30) not null,
+	  middlename varchar(30),
+	  lastname varchar(30),
+	  phone_no varchar(11) not null,
+	  email_id varchar(30) not null,
+	  date_of_birth date not null,
+	  address varchar(50) not null,
+	  zipcode int not null,
+	  account_type varchar(10) not null,
+	  balance decimal(20,4) not null check(balance > 5000.0),
+	  customer_id int not null,
+	  ifsc_code varchar(13));
+	  
+	  
+ insert into registration values( 138910000123456,'ABCDG0000H','saikumar','rao','pragada',7095079215,'xyz@gmail.com','1997-01-01','2-11,serilimgampally,financial district,hyd',508080,'savings',5001.999,19080001,'DBSH000389');
+	  
+	  
+	  
+drop table if exists Customer_details;
 
-create table CustomerDetails(custid int NOT NULL primary key,custname varchar(30) NOT NULL,phno int NOT NULL,
-email varchar(50),custaddress varchar(50) NOT NULL,custpan varchar(10) NOT NULL UNIQUE);
+drop table if exists Customer_login;
 
-create table Cust_Login(loginid int,
+drop table if exists Customer_transaction;
+
+drop table if exists Customer_account;
+
+create table Customer_details(cust_id int primary key,cust_name varchar(30) NOT NULL,phno int NOT NULL,
+email varchar(50) not null,cust_address varchar(50) NOT NULL,cust_pan varchar(10) NOT NULL UNIQUE);
+
+create table Customer_login(
+login_id int,
 pwd varchar(15) not null,
-lastlogin timestamp,
-constraint `log_key` foreign key(loginid) references CustomerDetails(custid));
+last_login_time timestamp,
+constraint `log_key` foreign key(login_id) references Customer_details(cust_id));
 
 
-create table Transaction(tid int not null AUTO_INCREMENT,fromacc int, toacc int,time timestamp,amount decimal(10,2),primary key(tid));
+create table Customer_transaction(
+tid int AUTO_INCREMENT primary key,
+fromacc int not null, 
+toacc int not null,
+trans_time timestamp not null,
+amount decimal(20,2),
+trans_type varchar(10) not null
+);
 
-create table Account(accnum int primary key,acctype varchar(20),custid int,constraint `fk_key` foreign key(custid) references CustomerDetails(custid) ); 
+create table Customer_account(
+acc_no int primary key,
+acc_type varchar(20) not null,
+cust_id int not null,
+amount decimal(20,4) not null,
+constraint `fk_key` foreign key(cust_id) references Customer_details(cust_id) ); 
